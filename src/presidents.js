239647874ce8075aea3013175e087994b65019cb
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-07-24 14:29:46 +0100

    Solved lab

--- src/presidents.js
+++ src/presidents.js
@@ -419,27 +419,37 @@ const presidents = [
 
 
 // Iteration 1 | Names of All Presidents - `map()`
-function getNames(presidentsArr) {}
-
-
+function getNames(presidentsArr) {
+  return presidentsArr.map(president => president.name);
+}
 
 
 // Iteration 2 | Democratic Presidents - `filter()`
-function getDemocraticPresidents(presidentsArr) {}
-
-
+function getDemocraticPresidents(presidentsArr) {
+  return presidentsArr.filter(president => president.party === "Democratic");
+}
 
 
 // Iteration 3 | Count Years in Office - reduce()
-function  countYearsInOffice(presidentsArr) {}
+function  countYearsInOffice(presidentsArr) {
+    return presidentsArr.reduce((accumulator, yearsInOffice) => {
 
+      if (yearsInOffice.leftOffice === null) return accumulator;
 
+      accumulator += yearsInOffice.leftOffice - yearsInOffice.tookOffice;
+
+      return accumulator;
+    }, 0);
+  }
 
 
 // Iteration 4 | Sort Presidents by Birth Year - `sort()`
-function sortPresidentsByBirthYear(presidentsArr) {}
-
-
+function sortPresidentsByBirthYear(presidentsArr) {
+  
+  return presidentsArr.sort (function (a, b){
+    return a.birthYear - b.birthYear;
+  });
+}
 
 
 // Bonus: Iteration 5 | Age at Inauguration - `map()`
